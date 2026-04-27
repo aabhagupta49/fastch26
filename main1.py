ddf924d9a7daa863eb40fffb8eb9bb156e94397f
Aabha Gupta <30920770+aabhagupta49@users.noreply.github.com>
2026-04-28 00:05:46 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import joblib
+
+app = FastAPI()
+
+@app.get("/")
+def predict_iris(sl:float, sw:float, pl:float, pw:float):
+    model = joblib.load("mymodel")
+
+    result = model.predict([[sl, sw, pl, pw]])
+    return {"prediction is ":int(result[0])}
